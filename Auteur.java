10a01736cf2d1d3da554359df229b6931e204470
Leo Saunier <leo.saunier@etu.univ-orleans.fr>
2024-02-01 09:17:52 +0100

    Ok

--- Auteur.java
+++ Auteur.java
@@ -1,3 +1,37 @@
+import java.util.Map ;
+import java.util.HashMap ;
+
 public class Auteur{
-    
+    private String nom;
+    private String citationTragedie;
+    private int qualiteTragedie;    
+    private String citationComedie;
+    private int qualiteComedie; 
+    private String citationDrame;
+    private int qualiteDrame;
+
+
+    public Auteur(String nom, int qualiteTragedie, String citationTragedie, int qualiteComedie, String citationComedie, int qualiteDrame, String citationDrame){
+        this.qualiteTragedie = qualiteTragedie;
+        this.citationTragedie = citationTragedie;
+        this.qualiteComedie = qualiteComedie;
+        this.citationComedie = citationComedie;
+        this.qualiteDrame = qualiteDrame;
+        this.citationDrame = citationDrame;
+        this.nom=nom;
+
+    } 
+    public int getQualiteTragedie(){
+        return this.qualiteTragedie;
+    }
+
+    public String getCitationTragedie(){
+        return this.citationTragedie;
+    }
+
+    @Override
+    public String toString() {
+        String s1 = "L'honorable ";
+        return s1.concat(this.nom);
+    }
 }
