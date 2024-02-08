69b1ed956534a7b8dca985b1f5d07440e47e0686
Leo Saunier <leo.saunier@etu.univ-orleans.fr>
2024-02-08 09:12:19 +0100

    Calcul des Scores

--- Journee.java
+++ Journee.java
@@ -1,20 +1,38 @@
-public class Journee{
-    private Style matin;
-    private Style apresMidi;
-    private Style soiree;
-    private int nbSpec;
+import java.util.Map;
+import java.util.HashMap;
 
-    public Journee(Style matin, Style apresMidi, Style soiree) {
-        this.matin = matin;
-        this.apresMidi = apresMidi;
-        this.soiree = soiree;
+public class Journee{
+    private Map<String,Integer> nbSpec;
+    private Map<String,Style> style;
+
+
+    public Journee(Style matin, int specMatin, Style apresMidi, int specApresMidi, Style soiree, int specSoiree) {
+        this.nbSpec = new HashMap<>();
+        this.style = new HashMap<>();
+        
+        this.nbSpec.put("Matin",specMatin);
+        this.nbSpec.put("ApresMidi",specApresMidi);
+        this.nbSpec.put("Soiree",specSoiree);
+
+        this.style.put("Matin",matin);
+        this.style.put("ApresMidi",apresMidi);
+        this.style.put("Soiree",soiree);
+    }
+
+    public Style getStyle(Periode periode) {
+        return this.style.get(periode.toString());
+    }
+
+    public int getNbSpec(Periode periode) {
+        return this.nbSpec.get(periode.toString());
     }
 
     public int scoreJournee( Auteur auteur ){
-        return scoreAuteur( auteur, this.matin)+scoreAuteur( auteur,  this.apresMidi)+scoreAuteur( auteur, this.soiree);
+        return this.scoreAuteur(auteur, Periode.MATIN)+this.scoreAuteur(auteur, Periode.APRESMIDI)+this.scoreAuteur(auteur, Periode.SOIREE);
     }
 
-    public int scoreAuteur(Auteur auteur, Journee epreuve){
-        return auteur.qualiteStyle(epreuve) * this.nbSpec;
+    public int scoreAuteur(Auteur auteur, Periode periode){
+        return auteur.qualiteStyle(this.style.get(periode.toString()))*this.getNbSpec(periode);
     }
+
 }
