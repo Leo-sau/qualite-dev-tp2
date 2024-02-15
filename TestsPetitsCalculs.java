a061aee424acef329bb9928ffb80b8baff1f3004
Leo Saunier <leo.saunier@etu.univ-orleans.fr>
2024-02-15 11:40:13 +0100

    test correct

--- TestsPetitsCalculs.java
+++ TestsPetitsCalculs.java
@@ -13,10 +13,10 @@ public class TestsPetitsCalculs {
     }
     @Test
     public void testMultiplication2() {
-    assertEquals(3252, 32 * 52);
+    assertEquals(1664, 32 * 52);
     }
     @Test
     public void testHexa() {
-    assertEquals(1024, 0x10 * 0x10);
+    assertEquals(256, 0x10 * 0x10);
     }
 }
